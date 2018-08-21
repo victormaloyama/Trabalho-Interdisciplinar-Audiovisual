d253d851874693e2b25fcbec1ac29631746cf70a
Victor Maloyama <victor.maloyama@gmail.com>
2018-08-21 20:38:52 -0300

    xml working

---
+++ www/js/requests.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "./home-data.json");
+	
+	xhr.addEventListener("load", function(){
+
+		var receitas = JSON.parse(xhr.responseText);
+		var conteudo = "";
+		
+		for (var i = 0; i < receitas.length; i++) {
+			conteudo += "<div class='receita-div'><h1>" + receitas[i].nome + "</h1>";
+			conteudo += "<img src='" + receitas[i].imagem + "' class='receita-img'>";
+			conteudo += "<h4 class='receita-tipo'>" + receitas[i].tipo + "</h4>";
+		}	
+			
+		$("#receitas-conteudo").append(conteudo);
+	});
+
+	xhr.send();
+});
