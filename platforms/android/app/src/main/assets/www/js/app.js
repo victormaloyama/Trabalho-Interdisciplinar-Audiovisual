edc8bd9b244893e03256593c23bcfdfe27a0576c
Victor Maloyama <victor.maloyama@gmail.com>
2018-09-18 20:21:34 -0300

    jQuery Mobile without css

--- platforms/android/app/src/main/assets/www/js/app.js
+++ platforms/android/app/src/main/assets/www/js/app.js
@@ -1,22 +1,32 @@
-$(".btn-scanqrcode").click(function(){
+$(".btn-scanqrcode").click(scannerInit);
+
+$(document).bind("swipe", function () {
+    $.event.special.swipe.horizontalDistanceThreshold = 180;
+    scannerInit();
+});
+
+function scannerInit(){
     cordova.plugins.barcodeScanner.scan(
-        function(result){
-            window.location.href = "content/"+result.text;
+        function(result) {
+            if (result==true) {
+                window.location.href = "content/" + result.text;
+            } else {
+                window.location.href = "#"
+            }
         },
         function(error) {
             alert("Falha no carregamento. " + error);
-        },
-        {
-            preferFrontCamera : false,
-            showFlipCameraButton : false,
-            showTorchButton : false,
+        }, {
+            preferFrontCamera: false,
+            showFlipCameraButton: false,
+            showTorchButton: false,
             torchOn: false,
-            saveHistory: false,
-            prompt : "Coloque o código QR do vídeo nesta área.",
-            resultDisplayDuration:0,
-            formats : "QR_CODE,PDF_417",
-            orientation : "portrait",
+            saveHistory: true,
+            prompt: "Coloque o código QR do vídeo nesta área.",
+            resultDisplayDuration: 0,
+            formats: "QR_CODE,PDF_417",
+            orientation: "portrait",
             disableSuccessBeep: true
         }
     );
-});
+}
